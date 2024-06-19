bb232e997bb2629f6dc73de596665cae85bba058
Fash Muyhee <fashmuyidavid@gmail.com>
2024-06-19 14:27:00 +0100

    project init with necessary packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    ["module-resolver", {
+      root: ["."],
+      alias: {
+        'components': './src/components',
+        'utils': './src/utils',
+        'assets': './src/assets',
+        'modules': './src/modules',
+        'hooks': './src/hooks',
+        'contexts': './src/contexts',
+        'routes': './src/routes',
+      }
+    }]
+  ],
 };
