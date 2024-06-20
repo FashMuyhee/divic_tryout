354b62589ee131e0d9602d4e27c8d1599c70460b
Fash Muyhee <fashmuyidavid@gmail.com>
2024-06-20 02:54:55 +0100

    add custom form validation hook

---
+++ src/hooks/useForm/useValidation.ts
@@ -0,0 +1,52 @@
+import {FieldValidation, FieldValidationParams} from './type';
+
+/**
+ * validate email string
+ * @param email
+ */
+const isValidEmail = (email: string) => {
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(email);
+};
+
+const isValidUrl = (url: string) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
+
+export const useValidation = (): FieldValidation => {
+  const validate = ({value, fieldType}: FieldValidationParams) => {
+    let error = null;
+    if (!value) {
+      error = 'Required';
+      return error;
+    }
+    switch (fieldType) {
+      case 'email':
+        if (isValidEmail(String(value))) {
+          error = null;
+        } else error = 'Invalid email';
+        break;
+      case 'password':
+        if (value.length < 8) {
+          error = '8 Character required';
+        }
+        break;
+      case 'url':
+        if (isValidUrl(String(value))) {
+          error = null;
+        } else error = 'Invalid url';
+        break;
+      default:
+        error = null;
+        break;
+    }
+    return error;
+  };
+
+  return {validate};
+};
