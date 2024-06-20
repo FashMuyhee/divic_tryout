293440966f4e243f9bd84796124792feadd4515b
Fash Muyhee <fashmuyidavid@gmail.com>
2024-06-20 03:29:04 +0100

    add disable color

--- src/components/commons/Button.tsx
+++ src/components/commons/Button.tsx
@@ -1,32 +1,27 @@
-import {
-  TouchableNativeFeedback,
-  StyleSheet,
-  ActivityIndicator,
-  View,
-} from 'react-native';
+import {TouchableNativeFeedback, StyleSheet, ActivityIndicator, View} from 'react-native';
 import React from 'react';
 import {ButtonProps} from './types';
 import {COLORS} from 'utils';
 import {Text} from './Text';
 
-export const Button = ({
-  onPress,
-  text,
-  isLoading,
-  textColor = COLORS.WHITE,
-  bgColor = COLORS.PRIMARY,style
-}: ButtonProps) => {
+export const Button = ({onPress, text, isLoading, textColor = COLORS.WHITE, bgColor = COLORS.PRIMARY, style, disabled = false}: ButtonProps) => {
+  const isDisabled = disabled || isLoading;
+
+  const bg = React.useMemo(() => {
+    if (disabled) return '#EAE7F2';
+    return bgColor;
+  }, [disabled]);
+
+  const color = React.useMemo(() => {
+    if (disabled) return COLORS.LIGHT_GREY;
+    return textColor;
+  }, [disabled]);
+
   return (
-    <TouchableNativeFeedback
-      onPress={onPress}
-      disabled={isLoading}>
-      <View style={[styles.wrapper, {backgroundColor: bgColor,},style]}>
+    <TouchableNativeFeedback onPress={onPress} disabled={isDisabled}>
+      <View style={[styles.wrapper, {backgroundColor: bg}, style]}>
         {isLoading && <ActivityIndicator size="small" color={COLORS.WHITE} />}
-        <Text
-          textAlign="center"
-          color={textColor}
-          fontSize={17}
-          fontWeight="bold">
+        <Text textAlign="center" color={color} fontSize={17} fontWeight="bold">
           {isLoading ? 'Loading' : text}
         </Text>
       </View>
